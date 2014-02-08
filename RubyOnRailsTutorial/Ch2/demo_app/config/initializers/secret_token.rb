eb7701dce0445601a1a2128cd9f85e1193262813
Mitchell Verter <mitchell.verter@gmail.com>
2014-02-08 15:37:06 -0500

    chapter 2

--- RubyOnRailsTutorial/Ch2/demo_app/config/initializers/secret_token.rb
+++ RubyOnRailsTutorial/Ch2/demo_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'e730f6bf3e28ecec61c0261a4853c1a36dd2dc8a9b62754ec93f199062cf21ff23f0d27de0478d3b0be4df9d604b04891c1c61de7e0a289da82b3ed76e40cb8d'
+DemoApp::Application.config.secret_token = 'a94b49f83418e133f03858f28b262e9ffafa3d535ab91543815954a28843cdf904dc9e64960520b062425fecd43692a539f6e3fe7bc6f0e51f446001a71a4380'
