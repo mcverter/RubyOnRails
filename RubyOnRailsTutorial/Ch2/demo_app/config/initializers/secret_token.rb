ee066378d8c38d675d2713ade2076ef99e4f9df9
Mitchell Verter <mitchell.verter@gmail.com>
2014-02-07 20:13:50 -0500

    adding chapter two

---
+++ RubyOnRailsTutorial/Ch2/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e730f6bf3e28ecec61c0261a4853c1a36dd2dc8a9b62754ec93f199062cf21ff23f0d27de0478d3b0be4df9d604b04891c1c61de7e0a289da82b3ed76e40cb8d'
