ea7b48bbf6b0c586481a58069e65e5cd82125deb
mcverter <mitchell.verter@gmail.com>
2014-02-21 23:27:09 -0500

    rewriting directories

---
+++ DesignPatterns/DerekBanas/5_FactoryPattern/BigUFOEnemyShip.java
@@ -0,0 +1,4 @@
+class BigUFOEnemyShip extends EnemyShip {
+    BigUFOEnemyShip (){
+    }
+}
---
+++ DesignPatterns/DerekBanas/5_FactoryPattern/EnemyShip.java
@@ -0,0 +1,22 @@
+public abstract class EnemyShip {
+    public String name;
+    public double amtDmg;
+
+    public String getName() {return name;}
+    public void setName(String newName){name=newName;}
+
+    public double getDamage() {return amtDmg;}
+    public void setDamage (double dmg){amtDmg = dmg;}
+
+    public void followHeroShip () {
+	System.out.println(getName() + "is following the hero");
+    }
+    public void displayEnemyShip () {
+	System.out.println(getName() + "is on the screen");
+    }
+    public void enemyShipShoots () {
+	System.out.println(getName() + "attacks and does " + 
+	    getDamage() + " damage");
+    }
+
+}
