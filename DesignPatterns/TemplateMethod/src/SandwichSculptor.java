44c10eb4a821a8573e48b9b49e517a52f4388b99
mcverter <mitchell.verter@gmail.com>
2014-02-23 09:04:08 -0500

    Template tutorial finished

---
+++ DesignPatterns/TemplateMethod/src/SandwichSculptor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mitchell on 2/23/14.
+ */
+public class SandwichSculptor {
+    public static void main(String[] args){
+        Hoagie c2_hoagie = new ItalianHoagie();
+        c2_hoagie.makeSandwich();
+
+        Hoagie c3_hoagie = new FalafelLafah();
+        c3_hoagie.makeSandwich();
+
+    }
+}
