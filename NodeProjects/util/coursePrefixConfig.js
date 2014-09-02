3be663c2ada947a32d42f7e658b0e963a68cb4e7
mcverter <mitchell.verter@gmail.com>
2014-09-02 14:41:45 -0400

    Adding Node Util for Prefixing Video Names

---
+++ NodeProjects/util/coursePrefixConfig.js
@@ -0,0 +1,23 @@
+module.exports = coursePrefixes;
+var coursePrefixes = [
+    {
+        dir: 'Agile_Design_Patterns',
+        prefix: 'AgileDesignPatterns'
+    },
+    {
+        dir: 'BACKTRACK',
+        prefix: 'Backtrack'
+    },
+    {
+        dir: 'clojure',
+        prefix: 'Clojure'
+    },
+    {
+        dir: '',
+        prefix: ''
+    },
+    {
+        dir: '',
+        prefix: ''
+    },
+];
