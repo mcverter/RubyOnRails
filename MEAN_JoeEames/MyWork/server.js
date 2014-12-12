c84e16c492a358b0ddf808534c30a7010a402685
mcverter <mitchell.verter@gmail.com>
2014-12-12 15:26:10 -0500

    Developing MEAN Joe Eames

--- MEAN_JoeEames/MyWork/server.js
+++ MEAN_JoeEames/MyWork/server.js
@@ -1,16 +1,15 @@
+
 var express = require('express'),
     env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     app = express();
 
-app.configure(function() {
-   app.set('views', __dirname + '/server/views');
-    app.set('view_engine', 'jade');
+app.set('views', HOME + '/server/views');
+app.set('view engine', 'jade');
+
+
+app.get('*', function(req, res) {
 });
 
-app.get('*', function(req, res){
-    res.render('index');
-});
-
-var port = 3000;
+var port = 3030;
 app.listen(port);
-console.log("Listening on port " + port);
+console.log("Listening to port " + port + "...")
