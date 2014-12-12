922157fcfa307429b0fde06fd1af7b3074ed747a
mcverter <mitchell.verter@gmail.com>
2014-12-12 15:59:21 -0500

    Developing MEAN Joe Eames

--- MEAN_JoeEames/MyWork/server.js
+++ MEAN_JoeEames/MyWork/server.js
@@ -1,10 +1,21 @@
 
 var express = require('express'),
     env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-    app = express();
+    app = express(),
+    stylus = require('stylus');
+
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
+
+app.use(stylus.middleware({src: __dirname + '/public',compile: compile}));
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('*', function(req, res) {
