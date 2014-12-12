c78b4add559e8baad1e9767bdff6accf6f24cc8b
mcverter <mitchell.verter@gmail.com>
2014-12-12 16:54:18 -0500

    Developing MEAN Joe Eames

--- MEAN_JoeEames/MyWork/server.js
+++ MEAN_JoeEames/MyWork/server.js
@@ -2,7 +2,8 @@
 var express = require('express'),
     env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     app = express(),
-    stylus = require('stylus');
+    stylus = require('stylus'),
+    mongoose=require('mongoose');
 
 function compile(str, path){
     return stylus(str).set('filename', path);
@@ -17,10 +18,22 @@ app.use(express.logger('dev'));
 app.use(stylus.middleware({src: __dirname + '/public',compile: compile}));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials' + req.params.partialPath);
+});
 
 app.get('*', function(req, res) {
+    res.render('index');
+});
+
+mongoose.connect('mongodb://localhost/multivision')
+db.on('error', console.error.bind('console', 'Connection error ...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
 });
 
 var port = 3030;
 app.listen(port);
+
+
 console.log("Listening to port " + port + "...");
