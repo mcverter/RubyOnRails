638921d2b7c42d34f2c811ecb1245a26d67f5026
mcverter <mitchell.verter@gmail.com>
2016-11-21 16:28:04 -0500

    adding undownloaded

--- plural/create_tables.sql
+++ plural/create_tables.sql
@@ -34,13 +34,13 @@ CREATE TABLE IF NOT EXISTS expected_file_counts (
 );
 
 
-COPY business FROM '/Users/mitchell_verter/Projects/plural/business.txt' DELIMITER ',' CSV;
-COPY creative FROM '/Users/mitchell_verter/Projects/plural/creative.txt' DELIMITER ',' CSV;
-COPY data FROM '/Users/mitchell_verter/Projects/plural/data.txt' DELIMITER ',' CSV;
-COPY itops FROM '/Users/mitchell_verter/Projects/plural/itops.txt' DELIMITER ',' CSV;
-COPY security FROM '/Users/mitchell_verter/Projects/plural/security.txt' DELIMITER ',' CSV;
-COPY software FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
+COPY business FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/business.txt' DELIMITER ',' CSV;
+COPY creative FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/creative.txt' DELIMITER ',' CSV;
+COPY data FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/data.txt' DELIMITER ',' CSV;
+COPY itops FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/itops.txt' DELIMITER ',' CSV;
+COPY security FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/security.txt' DELIMITER ',' CSV;
+COPY software FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/software.txt' DELIMITER ',' CSV;
 
-COPY current_file_counts FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
-COPY expected_file_counts FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
+COPY current_file_counts FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/current_file_counts.txt' DELIMITER ',' CSV;
+COPY expected_file_counts FROM '/Users/mitchell_verter/Projects/2014_CodeStudies/plural/expected_file_counts.txt' DELIMITER ',' CSV;
 
