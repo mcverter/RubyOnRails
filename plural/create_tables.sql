c0c99f70ff01d9eae4c740254d384722a18fc34e
unknown <you@example.com>
2016-11-21 20:28:42 -0500

    adding files for plurals

--- plural/create_tables.sql
+++ plural/create_tables.sql
@@ -34,13 +34,13 @@ CREATE TABLE IF NOT EXISTS expected_file_counts (
 );
 
 
-COPY business FROM '/Users/mitchell_verter/Projects/plural/business.txt' DELIMITER ',' CSV;
-COPY creative FROM '/Users/mitchell_verter/Projects/plural/creative.txt' DELIMITER ',' CSV;
-COPY data FROM '/Users/mitchell_verter/Projects/plural/data.txt' DELIMITER ',' CSV;
-COPY itops FROM '/Users/mitchell_verter/Projects/plural/itops.txt' DELIMITER ',' CSV;
-COPY security FROM '/Users/mitchell_verter/Projects/plural/security.txt' DELIMITER ',' CSV;
-COPY software FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
+COPY business FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\business.txt' DELIMITER ',' CSV;
+COPY creative FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\creative.txt' DELIMITER ',' CSV;
+COPY data FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\data.txt' DELIMITER ',' CSV;
+COPY itops FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\itops.txt' DELIMITER ',' CSV;
+COPY security FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\security.txt' DELIMITER ',' CSV;
+COPY software FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\software.txt' DELIMITER ',' CSV;
 
-COPY current_file_counts FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
-COPY expected_file_counts FROM '/Users/mitchell_verter/Projects/plural/software.txt' DELIMITER ',' CSV;
+COPY current_file_counts FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\software.txt' DELIMITER ',' CSV;
+COPY expected_file_counts FROM 'C:\ComputerScience_WINDOWS\2014_CodeStudies\plural\software.txt' DELIMITER ',' CSV;
 
